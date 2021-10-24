6c61b2c4aa6c17fa1516e0cb5c250aae5c6a1edb
Mahmoud Gamal <ma7moudgamal2014@gmail.com>
2021-10-24 20:15:24 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -23,95 +23,95 @@
  * 
 */
 //get all sections 
-const getSections = document.querySelectorAll('section')
+const getSections = document.querySelectorAll('section');
 
 //get navbar element
-const getNavList = document.getElementById('navbar__list')
+const getNavList = document.getElementById('navbar__list');
 
 
-                                            // start Build Navbar//
-
-//  build navbar according to sections 
-
-    let navBar = '';
-
-    // looping through getSections list 
-
-    getSections.forEach(sectionElement => {
-        navBar += `<li class='menu__link ${sectionElement.className}' data-link=${sectionElement.id}><a href="#${sectionElement.id}">${sectionElement.dataset.nav}</li>`;
-    });
-    // append all navBar <li> elements to the getNavList <ul> element 
-
-    getNavList.innerHTML = navBar;
+// start Build Navbar//
 
 //  build navbar according to sections 
 
-                                                // End Build Navbar //
+let navBar = '';
+
+// looping through getSections list 
+
+getSections.forEach(sectionElement => {
+  navBar += `<li class='menu__link ${sectionElement.className}' data-link=${sectionElement.id}><a href="#${sectionElement.id}">${sectionElement.dataset.nav}</li>`;
+});
+// append all navBar <li> elements to the getNavList <ul> element 
+
+getNavList.innerHTML = navBar;
+
+//  build navbar according to sections 
+
+// End Build Navbar //
 
 
-                                                 // Start Scrolling //
+// Start Scrolling //
 
 // scrolling to section according to nav link clicked
 
 getNavList.addEventListener('click', event => {
-    event.preventDefault();
-    let parent = ""
-    if(event.target.hasAttribute('data-link')){
-        parent= event.target;
-    } else{
-        parent=event.target.parentElement;
-    }
+  event.preventDefault();
+  let parent = ""
+  if (event.target.hasAttribute('data-link')) {
+    parent = event.target;
+  } else {
+    parent = event.target.parentElement;
+  }
 
-    //// Smooth scroll to anchor ID using scrollTO event
+  //// Smooth scroll to anchor ID using scrollTO event
 
-    let scrollTO = document.getElementById(parent.dataset.link)
-    scrollTO.scrollIntoView({block: 'start', behavior: 'smooth'})
-  })
+  let scrollTO = document.getElementById(parent.dataset.link);
+  scrollTO.scrollIntoView({ block: 'start', behavior: 'smooth' });
+});
 
 //  scrolling to section according to nav link clicked
 
-                                                   // End Scrolling //
+// End Scrolling //
 
-                                        // Start Add Active//
+// Start Add Active//
 
 // Add using the IntersectionObserver pattern
-  
-  // Setting an observer with options and Callback 
-  // for class 'active' to section when near top of viewport and remove active from other sections
 
-  const observer = new IntersectionObserver( entries => {
-    entries.forEach(entry => {
-      let navElement = document.querySelector(
-        `.menu__link[data-link='${entry.target.id}']`,
-      )
-      let targetSection = document.getElementById(entry.target.id)
-  
-      if (entry && entry.isIntersecting) {
-        navElement.classList.add('your-active-class')
-        targetSection.classList.add('your-active-class')
-      } else {
-        if (navElement.classList.contains('your-active-class')) {
-          navElement.classList.remove('your-active-class')
-        }
-  
-        if (targetSection.classList.contains('your-active-class')) {
-          targetSection.classList.remove('your-active-class')
-        }
+// Setting an observer with options and Callback 
+// for class 'active' to section when near top of viewport and remove active from other sections
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    let navElement = document.querySelector(
+      `.menu__link[data-link='${entry.target.id}']`,
+    )
+    let targetSection = document.getElementById(entry.target.id)
+
+    if (entry && entry.isIntersecting) {
+      navElement.classList.add('your-active-class')
+      targetSection.classList.add('your-active-class')
+    } else {
+      if (navElement.classList.contains('your-active-class')) {
+        navElement.classList.remove('your-active-class')
       }
-    })
-  }, {
-    root: null,
-    rootMargin: '0px',
-    threshold: 0.6,
-  })
-  getSections.forEach(el => {
-    observer.observe(document.getElementById(el.id))
-  })
 
-                                            // End Add Active
+      if (targetSection.classList.contains('your-active-class')) {
+        targetSection.classList.remove('your-active-class')
+      }
+    }
+  })
+}, {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.6,
+})
+getSections.forEach(el => {
+  observer.observe(document.getElementById(el.id))
+});
 
-                                            // Start Top Btn
-                                            
+// End Add Active
+
+// Start Top Btn
+
 // When the user clicks on the button, page is scrolled to the top of the document
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
@@ -120,4 +120,3 @@ function topFunction() {
 
                                              // End Top Btn
 
-  
